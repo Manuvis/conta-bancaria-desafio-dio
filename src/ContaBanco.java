529b2b457992d58fb632777f00895835904e9b41
Manuella Jatobá <manuellafigueiredojatoba@gmail.com>
2023-09-10 17:17:37 -0300

    primeira versão do progra que simula a conta bancária

--- src/ContaBanco.java
+++ src/ContaBanco.java
@@ -1,6 +1,35 @@
 public class ContaBanco {
 
-    public static void main(String[] args) {
+     int numeroConta;
+     String agencia;
+     String nomeCliente;
+     float saldo;
 
+    public void criarConta (){
+        System.out.println("\"Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia + ", conta "+ numeroConta +  "e seu saldo " + saldo + " já está disponível para saque\".");
+    }
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+    public String getAgencia() {
+        return agencia;
+    }
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+    public float getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
     }
 }
--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,2 +1,34 @@
+import java.util.Scanner;
 public class ContaTerminal {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner input = new Scanner(System.in);
+        ContaBanco contaBancaria = new ContaBanco();
+
+        System.out.println("Qual seu primeiro nome ?");
+        String priemiroNome = input.next();
+        System.out.println("Qual seu primeiro nome ?");
+        String sobrenome = input.next();
+        contaBancaria.nomeCliente = priemiroNome + sobrenome;
+
+        System.out.println("Qual o numero da conta ?");
+        int numero = input.nextInt();
+        contaBancaria.numeroConta = numero;
+
+        System.out.println("Qual o numero da agência ? ");
+        String agencia = input.next();
+        contaBancaria.agencia = agencia;
+
+        System.out.println("Qual o saldo da sua conta ? ");
+        float saldo = input.nextFloat();
+        contaBancaria.saldo = saldo;
+
+
+        contaBancaria.criarConta();
+
+
+        input.close();
+    }
+
 }
